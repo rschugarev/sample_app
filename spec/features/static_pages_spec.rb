294be75d7ff5e48944cf8af4b385b4f71508baec
Roman <romarv07@gmail.com>
2016-07-06 16:22:48 +0300

    Finish static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,9 +1,42 @@
 require 'spec_helper'
 
-describe "Home page" do
+describe "Static pages" do
 
+describe "Home page" do
   it "should have the content 'Sample App'" do
     visit '/static_pages/home'
-    expect(page).to have_content('Sample App')
+    expect(page).to have_content('home')
   end
+
+  it "should have the title 'Home'" do
+    visit '/static_pages/home'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  end
+end
+
+describe "Help page" do
+  it "should have the content 'help'" do
+    visit '/static_pages/help'
+    expect(page).to have_content('help')
+  end
+
+  it "should have the title 'Help'" do
+    visit '/static_pages/help'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  end
+end
+
+describe "About page" do
+  it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+  end
+
+  it "should have the title 'About Us'" do
+    visit '/static_pages/about'
+    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+  end
+
+end
+
 end
